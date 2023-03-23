4c8383e42d9eea717e6eb26fc2c2c61b2cd411dd
Streafe <streafe@gmail.com>
2023-03-23 16:42:20 +0100

    webpack changes

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,7 +1,14 @@
-import { createWebpackConfigAsync } from "@expo/webpack-config";
+import createExpoWebpackConfigAsync from "@expo/webpack-config";
 
 export default async function (env, argv) {
-  const config = await createWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
+
+  // Add resolve property with the specified extensions
+  config.resolve = {
+    ...config.resolve, // Keep any existing resolve configurations
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  };
+
   return config;
 }
