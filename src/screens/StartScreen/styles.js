ac87edeab2ddcc8c219ca7f1404a5f65bffc3d4c
Streafe <streafe@gmail.com>
2023-03-23 14:44:22 +0100

    Create styles.js

---
+++ src/screens/StartScreen/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+    backgroundColor: "#FDFDFD",
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+export default styles;
