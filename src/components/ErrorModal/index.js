71b4478b96c9ecc0e2b90efa622304fabe055aee
Streafe <streafe@gmail.com>
2023-03-28 13:32:26 +0200

    Make ErrorModal its own component

---
+++ src/components/ErrorModal/index.js
@@ -0,0 +1,119 @@
+import React from "react";
+import {
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const ErrorModal = ({ selectedError, modalVisible, setModalVisible }) => (
+  <Modal
+    animationType="fade"
+    transparent={true}
+    visible={modalVisible}
+    onRequestClose={() => {
+      setModalVisible(!modalVisible);
+    }}
+  >
+    <View style={modalStyle.overlay}>
+      <View style={modalStyle.centeredView}>
+        <View style={modalStyle.modalView}>
+          <Text style={modalStyle.modalTitle}>Error details</Text>
+          <ScrollView style={modalStyle.scrollView}>
+            {selectedError.map((error) => (
+              <View key={error.id} style={modalStyle.errorItem}>
+                <Text style={modalStyle.errorId}>ID: {error.id}</Text>
+                <Text style={modalStyle.errorMessage}>
+                  {error.errorMessage}
+                </Text>
+              </View>
+            ))}
+          </ScrollView>
+
+          <TouchableOpacity
+            style={{ ...modalStyle.openButton, backgroundColor: "#2196F3" }}
+            onPress={() => {
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <Text style={modalStyle.textStyle}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  </Modal>
+);
+
+const modalStyle = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    width: width * 0.8,
+    backgroundColor: "white",
+    borderRadius: 12,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  scrollView: {
+    width: "100%",
+    maxHeight: 200,
+    marginBottom: 15,
+  },
+  errorItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  errorId: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  errorMessage: {
+    fontSize: 16,
+  },
+  openButton: {
+    borderRadius: 6,
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+    elevation: 2,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
+
+export default ErrorModal;
--- src/components/NetworkScanner/index.js
+++ src/components/NetworkScanner/index.js
@@ -23,6 +23,8 @@ import { MaterialIcons } from "@expo/vector-icons";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 
+import ErrorModal from "../ErrorModal";
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -39,6 +41,10 @@ function NetworkScanner() {
   const ROBOT_COMMAND = "rpc/get_agv_data";
 
   const [robotData, setRobotData] = useState([]);
+
+  /* Error modal */
+  const [selectedError, setSelectedError] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     setRobotData([
@@ -286,116 +292,6 @@ function NetworkScanner() {
     </Animated.View>
   );
 
-  const { width, height } = Dimensions.get("window");
-
-  const [selectedError, setSelectedError] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const renderModal = () => (
-    <Modal
-      animationType="fade"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => {
-        setModalVisible(!modalVisible);
-      }}
-    >
-      <View style={modalStyle.overlay}>
-        <View style={modalStyle.centeredView}>
-          <View style={modalStyle.modalView}>
-            <Text style={modalStyle.modalTitle}>Error details</Text>
-            <ScrollView style={modalStyle.scrollView}>
-              {selectedError.map((error) => (
-                <View key={error.id} style={modalStyle.errorItem}>
-                  <Text style={modalStyle.errorId}>ID: {error.id}</Text>
-                  <Text style={modalStyle.errorMessage}>
-                    {error.errorMessage}
-                  </Text>
-                </View>
-              ))}
-            </ScrollView>
-
-            <TouchableOpacity
-              style={{ ...modalStyle.openButton, backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <Text style={modalStyle.textStyle}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </Modal>
-  );
-
-  const modalStyle = StyleSheet.create({
-    overlay: {
-      flex: 1,
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-    },
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 22,
-    },
-    modalView: {
-      width: width * 0.8,
-      backgroundColor: "white",
-      borderRadius: 12,
-      padding: 35,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-    },
-    modalTitle: {
-      marginBottom: 15,
-      textAlign: "center",
-      fontWeight: "bold",
-      fontSize: 20,
-    },
-    scrollView: {
-      width: "100%",
-      maxHeight: 200,
-      marginBottom: 15,
-    },
-    errorItem: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      paddingHorizontal: 10,
-      paddingVertical: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: "#ccc",
-    },
-    errorId: {
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    errorMessage: {
-      fontSize: 16,
-    },
-    openButton: {
-      borderRadius: 6,
-      paddingHorizontal: 5,
-      paddingVertical: 3,
-      elevation: 2,
-    },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center",
-      fontSize: 16,
-    },
-  });
-
   const renderItem = ({ item }) => {
     const batteryLevel = (battery_capacity) => {
       if (battery_capacity > 80) return "battery-full";
@@ -530,7 +426,11 @@ function NetworkScanner() {
   return (
     <>
       {renderNotification()}
-      {renderModal()}
+      <ErrorModal
+        selectedError={selectedError}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
       <View style={styles.container}>
         {isLoading ? (
           <ActivityIndicator size="large" color="#0000ff" />
