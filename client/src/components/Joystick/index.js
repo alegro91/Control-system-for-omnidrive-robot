952d054ff9825cad294b24cde17996e5a0f0ce85
Streafe <streafe@gmail.com>
2023-04-24 13:41:58 +0200

    Fixed joystick movement not working

--- client/src/components/Joystick/index.js
+++ client/src/components/Joystick/index.js
@@ -1,12 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { Svg, Circle } from "react-native-svg";
 
 export default function Joystick({ robotIp, steeringType, driveMode }) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const debouncedSendJoystickDataRef = useRef(debounce(sendJoystickData, 100));
+  const debouncedSendJoystickDataRef = useRef(debounce(sendJoystickData, 10));
+  const viewRef = useRef(null);
 
   function sendJoystickData(x, y, normalizedDistance) {
+    /*
     const url = `http://${robotIp}:5656/control`;
     const data = {
       x: x,
@@ -25,6 +27,7 @@ export default function Joystick({ robotIp, steeringType, driveMode }) {
     }).catch((error) => {
       console.error("Error:", error);
     });
+    */
   }
 
   function debounce(func, wait) {
@@ -41,6 +44,8 @@ export default function Joystick({ robotIp, steeringType, driveMode }) {
 
   const handleStart = (event) => {
     event.preventDefault();
+    event.target.addEventListener("mousemove", handleMove, { passive: false });
+    event.target.addEventListener("mouseup", handleEnd, { passive: false });
   };
 
   const handleTouchStart = (event) => {
@@ -51,11 +56,18 @@ export default function Joystick({ robotIp, steeringType, driveMode }) {
   };
 
   const handleMove = (event) => {
-    const { pageX, pageY, target } = event.nativeEvent;
+    const { pageX, pageY, target, touches } = event.nativeEvent;
     const { left, top } = target.getBoundingClientRect();
 
-    let x = pageX - left - 100;
-    let y = pageY - top - 100;
+    let x, y;
+
+    if (touches && touches.length > 0) {
+      x = touches[0].pageX - left - 100;
+      y = touches[0].pageY - top - 100;
+    } else {
+      x = pageX - left - 100;
+      y = pageY - top - 100;
+    }
 
     // Calculate the distance from the center.
     const distance = Math.sqrt(x * x + y * y);
@@ -79,21 +91,53 @@ export default function Joystick({ robotIp, steeringType, driveMode }) {
     sendJoystickData(0, 0, 0);
   };
 
+  useEffect(() => {
+    if (viewRef.current) {
+      viewRef.current.addEventListener("touchstart", handleTouchStart, {
+        passive: false,
+      });
+      viewRef.current.addEventListener("touchend", handleEnd, {
+        passive: false,
+      });
+      viewRef.current.addEventListener("touchcancel", handleEnd, {
+        passive: false,
+      });
+      viewRef.current.addEventListener("touchmove", handleMove, {
+        passive: false,
+      });
+
+      viewRef.current.addEventListener("mousedown", handleStart, {
+        passive: false,
+      });
+      viewRef.current.addEventListener("mouseup", handleEnd, {
+        passive: false,
+      });
+      viewRef.current.addEventListener("mousemove", handleMove, {
+        passive: false,
+      });
+    }
+
+    return () => {
+      if (viewRef.current) {
+        viewRef.current.removeEventListener("touchstart", handleTouchStart);
+        viewRef.current.removeEventListener("touchend", handleEnd);
+        viewRef.current.removeEventListener("touchcancel", handleEnd);
+        viewRef.current.removeEventListener("touchmove", handleMove);
+
+        viewRef.current.removeEventListener("mousedown", handleStart);
+        viewRef.current.removeEventListener("mouseup", handleEnd);
+        viewRef.current.removeEventListener("mousemove", handleMove);
+      }
+    };
+  }, []);
+
   return (
-    <View
-      onStartShouldSetResponder={handleStart}
-      onResponderGrant={handleStart}
-      onResponderMove={handleMove}
-      onResponderRelease={handleEnd}
-      onResponderTerminate={handleEnd}
-      onTouchStart={handleTouchStart}
-      style={styles.viewContainer}
-    >
-      <Svg height="200" width="200" style={styles.joystickContainer}>
-        <Circle cx="100" cy="100" r="100" fill="rgba(200, 200, 200, 0.3)" />
+    <View ref={viewRef} style={styles.viewContainer}>
+      <Svg height="400" width="400" style={styles.joystickContainer}>
+        <Circle cx="200" cy="200" r="100" fill="rgba(200, 200, 200, 0.3)" />
         <Circle
-          cx={100 + position.x}
-          cy={100 + position.y}
+          cx={200 + position.x}
+          cy={200 + position.y}
           r="50"
           fill="rgba(80, 80, 80, 0.7)"
         />
