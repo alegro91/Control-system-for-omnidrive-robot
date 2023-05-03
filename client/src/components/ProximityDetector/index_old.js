af3048c479559d7b30329aeb391d724daa9043d3
THOMAS-PC\thoma <thomasystenes@gmail.com>
2023-05-03 02:25:05 +0200

    Made prebuild for Android.
    Needed to expose native code for the react-native-ble-plx library.
    Could not build iOS version, as that requires a macOS device.

--- client/src/components/ProximityDetector/index_old.js
+++ client/src/components/ProximityDetector/index_old.js
@@ -1,8 +1,12 @@
 import { BleManager } from "react-native-ble-plx";
 
-class BluetoothLE {
-  manager = new BleManager();
+//Class for handeling proximity detection of Bluetooth devices
+class ProximityDetectorBT {
+  ProximityDetector = new BleManager();
 
+  //*
+  //name - the name of the Bluetooth device being searched for.
+  //*
   scanForDevice(name) {
     // Scan for nearby Bluetooth LE devices
     this.manager.startDeviceScan(null, null, (error, device) => {
@@ -13,7 +17,7 @@ class BluetoothLE {
 
       // Check if the device is the one you're looking for
       if (device.name === name) {
-        // Retrieve the RSSI value
+        // Get RSSI value
         device.readRSSI().then((rssi) => {
           console.log(`RSSI for ${device.name}: ${rssi}`);
         });
@@ -21,9 +25,10 @@ class BluetoothLE {
     });
   }
 
+  //Test function to ensure the import worked as intended without having to think about the rest of the logic for now.
   addNumbers(num1, num2) {
     return num1 + num2;
   }
 }
 
-export default BluetoothLE;
+export default ProximityDetectorBT;
