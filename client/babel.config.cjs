a501dd0c63f0ffc8e20204c2c92454e024542fdd
Streafe <streafe@gmail.com>
2023-05-17 23:18:49 +0200

    Update of eas.json to fix eas build

--- client/babel.config.cjs
+++ client/babel.config.cjs
@@ -2,10 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    /*
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
     ],
+    */
   };
 };
